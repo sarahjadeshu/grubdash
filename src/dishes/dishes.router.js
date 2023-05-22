8c09d72e5f6ce40db8e8cf642d8ae426df2c4da7
Sarah Shu <65093042+sarahjadeshu@users.noreply.github.com>
2023-05-21 21:09:06 -0700

    Update dishes.router.js
    Delete todo comments

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -2,7 +2,6 @@ const router = require("express").Router({ mergeParams: true });
 const controller = require("./dishes.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-// TODO: Implement the /dishes routes needed to make the tests pass
 router.route("/:dishId").get(controller.read).put(controller.update).all(methodNotAllowed);
 router.route("/").get(controller.list).post(controller.create).all(methodNotAllowed);
 
